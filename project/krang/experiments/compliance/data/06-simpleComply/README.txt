cc98bc26607cd08f55bc570fb0b290f5a49ea401
munzir <munzir_z@hotmail.com>
2018-06-28 19:40:33 -0400

    remove unnecessary files

--- project/krang/experiments/compliance/data/06-simpleComply/README.txt
+++
@@ -1,1 +0,0 @@
-See the "2013-06-20 - Simple compliance" video in Documentation/Experiments folder @thebrain:/home/git/doc/krang.git.
--- project/krang/experiments/compliance/data/debugging-amplitude/graph.m
+++
@@ -1,6 +0,0 @@
-subplot(2,1,1); colors = {'k', 'b', 'r', 'g', 'c', 'm'}; for i = 1 : 3, plot(1:3:size(bla,1), bla(2:3:end,i), colors{i}); hold on; end                        
-title([subject, ' - Unbiased with 0'], 'FontSize', 20) 	
-legend('x', 'y', 'z');
-subplot(2,1,2); colors = {'k', 'b', 'r', 'g', 'c', 'm'}; for i = 1 : 2, plot(1:3:size(bla,1), bla(3:3:end,i), colors{i}); hold on; end                        
-title([subject, ' - Unbiased with initial value'], 'FontSize', 20) 
-legend('x', 'y');
